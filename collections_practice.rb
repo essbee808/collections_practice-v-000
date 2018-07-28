a55d32f3f61afcf12f49123954725d049e21708b
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-28 23:30:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,7 +45,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|
+  array.each.collect do |element|
     if element == "hand"
       "#{element}s"
     elsif element == "knee"
